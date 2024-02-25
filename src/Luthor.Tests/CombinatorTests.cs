79aafde285fe013d070401a9816c4e1a8b59f929
Mark Lauter <mark@marklauter.com>
2024-02-24 23:45:05 -0500

    revived dead code

--- src/Luthor.Tests/CombinatorTests.cs
+++ src/Luthor.Tests/CombinatorTests.cs
@@ -1,38 +1,36 @@
-﻿//using System.Diagnostics.CodeAnalysis;
+﻿using Luthor.Lexers;
+using System.Diagnostics.CodeAnalysis;
 
+namespace Luthor.Tests;
 
-//namespace Luthor.Tests;
+[ExcludeFromCodeCoverage]
+public sealed class CombinatorTests
+{
+    [Theory]
+    [InlineData("1", '1', '2', true)]
+    [InlineData("2", '1', '2', true)]
+    [InlineData("a", '1', '2', false)]
+    public void Or(string source, char lch, char rch, bool expectedMatch)
+    {
+        var lexer = CharacterClasses
+            .Is(lch)
+            .Or(CharacterClasses.Is(rch));
 
-//[ExcludeFromCodeCoverage]
-//public sealed class CombinatorTests
-//{
-//    [Theory]
-//    [InlineData("1", '1', '2', true)]
-//    [InlineData("2", '1', '2', true)]
-//    [InlineData("a", '1', '2', false)]
-//    public void Or(string value, char lch, char rch, bool expectedMatch)
-//    {
-//        var segment = new Segment(value);
-//        var patternMatcher = Character
-//            .IsEqual(lch)
-//            .Or(Character.IsEqual(rch));
+        var lexeme = lexer(source);
+        Assert.Equal(expectedMatch, lexeme.Success);
+    }
 
-//        var match = patternMatcher(segment);
-//        Assert.Equal(expectedMatch, match.Success);
-//    }
+    [Theory]
+    [InlineData("11", '1', '2', false)]
+    [InlineData("22", '1', '2', false)]
+    [InlineData("11", '1', '1', true)]
+    public void Then(string source, char lch, char rch, bool expectedMatch)
+    {
+        var lexer = CharacterClasses
+            .Is(lch)
+            .Then(CharacterClasses.Is(rch));
 
-//    [Theory]
-//    [InlineData("11", '1', '2', false)]
-//    [InlineData("22", '1', '2', false)]
-//    [InlineData("11", '1', '1', true)]
-//    public void Then(string value, char lch, char rch, bool expectedMatch)
-//    {
-//        var segment = new Segment(value);
-//        var patternMatcher = Character
-//            .IsEqual(lch)
-//            .Then(Character.IsEqual(rch));
-
-//        var match = patternMatcher(segment);
-//        Assert.Equal(expectedMatch, match.Success);
-//    }
-//}
+        var lexeme = lexer(source);
+        Assert.Equal(expectedMatch, lexeme.Success);
+    }
+}
