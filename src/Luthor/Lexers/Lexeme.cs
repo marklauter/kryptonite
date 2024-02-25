dc5ac48339badec7afe8d5154cf78d6d57b850b3
Mark Lauter <mark@marklauter.com>
2024-02-24 23:19:03 -0500

    fixed Lexeme length, moved Lexeme<TToken> to own file

--- src/Luthor/Lexers/Lexeme.cs
+++ src/Luthor/Lexers/Lexeme.cs
@@ -29,7 +29,7 @@ public class Lexeme
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Lexeme Hit(Segment match, Segment remainder)
     {
-        return new Lexeme(match, remainder, 1);
+        return new Lexeme(match, remainder, match.Length - remainder.Length);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -39,15 +39,21 @@ public class Lexeme
             ? new Lexeme(match, remainder, length)
             : throw new ArgumentOutOfRangeException(nameof(length));
     }
-};
 
-public sealed class Lexeme<TToken>(Lexeme lexeme, TToken token)
-    : Lexeme(
-        lexeme?.Match ?? throw new ArgumentNullException(nameof(lexeme)),
-        lexeme?.Remainder ?? throw new ArgumentNullException(nameof(lexeme)),
-        lexeme?.Length ?? throw new ArgumentNullException(nameof(lexeme)))
-    where TToken : Enum
-{
-    public TToken Token { get; } = token;
-    public string Value => Match.Value[Match.Offset..Length];
-}
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator string(Lexeme lexeme)
+    {
+        return lexeme.ToString();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlySpan<char> AsSpan()
+    {
+        return Match.AsSpan()[0..Length];
+    }
+
+    public override string ToString()
+    {
+        return ((string)Match)[0..Length];
+    }
+};
