0164fd0cc2b5e213f7f91f2b8302db84ab5b3e5f
Mark Lauter <mark@marklauter.com>
2024-02-24 23:37:39 -0500

    added RegexClasses

---
+++ src/Luthor/Lexers/RegexClasses.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Luthor.Lexers;
+
+public static class RegexClasses
+{
+    // todo: patterns require wrap in \G(?:{pattern})
+    public static Lexer MatchRegex(Regex pattern)
+    {
+        return segment =>
+        {
+            var match = pattern.Match(segment);
+            return match.Success
+                ? Lexeme.Hit(segment, segment.Advance(match.Length), match.Length)
+                : Lexeme.Miss(segment);
+        };
+    }
+}
