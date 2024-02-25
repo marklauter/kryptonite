ec446da82a6bac84488299539f6f0a576e42055a
Mark Lauter <mark@marklauter.com>
2024-02-24 23:19:27 -0500

    split lexer defs to own files

---
+++ src/Luthor/Lexers/Lexer{TToken}.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Luthor.Lexers;
+
+public sealed class Lexer<TToken>(
+    Lexer lexer,
+    TToken token,
+    bool ignored)
+    where TToken : Enum
+{
+    private readonly Lexer lexer = lexer ?? throw new ArgumentNullException(nameof(lexer));
+    private readonly TToken token = token;
+    private readonly bool ignored = ignored;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Lexer(Lexer lexer, TToken token)
+        : this(lexer, token, false)
+    { }
+
+    public sealed record MatchResult(
+        Lexeme Lexeme,
+        TToken Token,
+        bool Ignored)
+    {
+        public bool Success => Lexeme.Success;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public MatchResult Match(Segment segment)
+    {
+        return new MatchResult(
+            lexer.Invoke(segment),
+            token,
+            ignored);
+    }
+}
