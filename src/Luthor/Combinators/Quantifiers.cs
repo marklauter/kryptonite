598a49e5f95122b68de4b0421b4770a2ab6e7004
Mark Lauter <mark@marklauter.com>
2024-02-25 18:52:10 -0500

    split combinators into quantifiers and sequences, added Add, ZeroOrMore, ZeroOrOne, OneOrMore, Exactly, AtLeast, AtLeastButNoMore

---
+++ src/Luthor/Combinators/Quantifiers.cs
@@ -0,0 +1,72 @@
+﻿using Luthor.Lexers;
+
+namespace Luthor.Combinators;
+
+public static class Quantifiers
+{
+    // *
+    public static Lexer ZeroOrMore(this Lexer lexer)
+    {
+        ArgumentNullException.ThrowIfNull(lexer);
+
+        return segment =>
+        {
+            var start = segment;
+            var lexeme = lexer(segment);
+            while (lexeme.Success)
+            {
+                lexeme = lexer(lexeme.Remainder);
+            }
+
+            return Lexeme.Hit(start, lexeme.Remainder);
+        };
+    }
+
+    // ?
+    public static Lexer ZeroOrOne(this Lexer lexer)
+    {
+        ArgumentNullException.ThrowIfNull(lexer);
+
+        return segment =>
+        {
+            var lexeme = lexer(segment);
+            return lexeme.Success
+                ? lexeme
+                : Lexeme.Hit(segment, segment); // a hit without consuming input
+        };
+    }
+
+    // +
+    public static Lexer OneOrMore(this Lexer lexer)
+    {
+        ArgumentNullException.ThrowIfNull(lexer);
+
+        return segment =>
+        {
+            var start = segment;
+            var lexeme = lexer(segment);
+            if (lexeme.Success) // meet the match one condition correctly
+            {
+                while (lexeme.Success) // match more so we get the longest match and pass lexeme.Remainder to Hit
+                {
+                    lexeme = lexer(lexeme.Remainder);
+                }
+
+                return Lexeme.Hit(start, lexeme.Remainder);
+            }
+
+            return Lexeme.Miss(start);
+        };
+    }
+
+    // {n}
+    public static Lexer Exactly(int n) => throw new NotImplementedException(nameof(Exactly));
+
+    // {n,}
+    public static Lexer AtLeast(int n) =>
+        // todo: look at OneOrMore for guidance
+        throw new NotImplementedException(nameof(AtLeast));
+
+    // {n, m}
+    public static Lexer AtLeastButNoMore(int n, int m) => throw new NotImplementedException(nameof(AtLeastButNoMore));
+}
---
+++ src/Luthor/Combinators/Sequences.cs
@@ -0,0 +1,57 @@
+﻿using Luthor.Lexers;
+
+namespace Luthor.Combinators;
+
+public static class Sequences
+{
+    // |
+    public static Lexer Or(this Lexer left, Lexer right)
+    {
+        ArgumentNullException.ThrowIfNull(left);
+        ArgumentNullException.ThrowIfNull(right);
+
+        return segment =>
+        {
+            var lexeme = left(segment);
+            return lexeme.Success
+                ? lexeme
+                : right(segment);
+        };
+    }
+
+    // i guess 'and' is implicit in regex
+    public static Lexer And(this Lexer first, Lexer second)
+    {
+        ArgumentNullException.ThrowIfNull(first);
+        ArgumentNullException.ThrowIfNull(second);
+
+        return segment =>
+        {
+            var lexeme = first(segment);
+            if (lexeme.Success)
+            {
+                lexeme = second(lexeme.Remainder);
+                if (lexeme.Success)
+                {
+                    return Lexeme.Hit(segment, lexeme.Remainder);
+                }
+            }
+
+            return Lexeme.Miss(segment);
+        };
+    }
+
+    public static Lexer Then(this Lexer first, Lexer second)
+    {
+        ArgumentNullException.ThrowIfNull(first);
+        ArgumentNullException.ThrowIfNull(second);
+
+        return segment =>
+        {
+            var lexeme = first(segment);
+            return lexeme.Success
+                ? second(lexeme.Remainder)
+                : lexeme;
+        };
+    }
+}
