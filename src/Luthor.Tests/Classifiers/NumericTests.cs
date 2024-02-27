5438e5791c7d1fe47dc27a9212fab28520e7f046
Mark Lauter <mark@marklauter.com>
2024-02-26 23:11:11 -0500

    removed non-combinatory number lexers

--- src/Luthor.Tests/Classifiers/NumericTests.cs
+++ src/Luthor.Tests/Classifiers/NumericTests.cs
@@ -86,49 +86,6 @@ public sealed class NumericTests
     }
 
     [Theory]
-    [InlineData("1", true)]
-    [InlineData("12", true)]
-    [InlineData("123456", true)]
-    [InlineData("+1", true)]
-    [InlineData("-1", true)]
-    [InlineData("+123", true)]
-    [InlineData("-123", true)]
-
-    [InlineData("1.0", true)]
-    [InlineData("12.0", true)]
-    [InlineData("123456.0", true)]
-    [InlineData("+1.0", true)]
-    [InlineData("-1.0", true)]
-    [InlineData("+123.0", true)]
-    [InlineData("-123.0", true)]
-
-    [InlineData("1.0E1", true)]
-    [InlineData("1.0e1", true)]
-    [InlineData("+1.0E1", true)]
-    [InlineData("-1.0e1", true)]
-    [InlineData("1.0E+1", true)]
-    [InlineData("1.0e-1", true)]
-
-    [InlineData("-", false)]
-    [InlineData("+", false)]
-    [InlineData("a", false)]
-    [InlineData(" ", false)]
-    public void IsInteger3(string source, bool expectedSuccess)
-    {
-        var lexeme = Number.IsInteger3(source);
-        Assert.Equal(expectedSuccess, lexeme.Matched);
-        if (expectedSuccess)
-        {
-            if (source.Contains('.'))
-            {
-                source = source.Split('.')[0];
-            }
-
-            Assert.Equal(source, lexeme);
-        }
-    }
-
-    [Theory]
     [InlineData("1", false)]
     [InlineData("12", false)]
     [InlineData("123456", false)]
@@ -159,50 +116,6 @@ public sealed class NumericTests
     public void IsFloatingPoint(string source, bool expectedSuccess)
     {
         var lexeme = Number.IsFloatingPoint(source);
-        Assert.Equal(expectedSuccess, lexeme.Matched);
-        if (lexeme.Matched)
-        {
-            source = source.Contains('E')
-                ? source.Split('E')[0]
-                : source.Contains('e')
-                    ? source.Split('e')[0]
-                    : source;
-
-            Assert.Equal(source, lexeme);
-        }
-    }
-
-    [Theory]
-    [InlineData("1", false)]
-    [InlineData("12", false)]
-    [InlineData("123456", false)]
-    [InlineData("+1", false)]
-    [InlineData("-1", false)]
-    [InlineData("+123", false)]
-    [InlineData("-123", false)]
-
-    [InlineData("1.0", true)]
-    [InlineData("12.0", true)]
-    [InlineData("123456.0", true)]
-    [InlineData("+1.0", true)]
-    [InlineData("-1.0", true)]
-    [InlineData("+123.0", true)]
-    [InlineData("-123.0", true)]
-
-    [InlineData("1.0E1", true)]
-    [InlineData("1.0e1", true)]
-    [InlineData("+1.0E1", true)]
-    [InlineData("-1.0e1", true)]
-    [InlineData("1.0E+1", true)]
-    [InlineData("1.0e-1", true)]
-
-    [InlineData("-", false)]
-    [InlineData("+", false)]
-    [InlineData("a", false)]
-    [InlineData(" ", false)]
-    public void IsFloatingPoint2(string source, bool expectedSuccess)
-    {
-        var lexeme = Number.IsFloatingPoint2(source);
         Assert.Equal(expectedSuccess, lexeme.Matched);
         if (lexeme.Matched)
         {
@@ -275,46 +188,6 @@ public sealed class NumericTests
 
     [InlineData("1E1", false)]
 
-    [InlineData("1.0E1", true)]
-    [InlineData("1.0e1", true)]
-    [InlineData("+1.0E1", true)]
-    [InlineData("-1.0e1", true)]
-    [InlineData("1.0E+1", true)]
-    [InlineData("1.0e-1", true)]
-
-    [InlineData("-", false)]
-    [InlineData("+", false)]
-    [InlineData("a", false)]
-    [InlineData(" ", false)]
-    public void IsScientificNotation2(string source, bool expectedSuccess)
-    {
-        var lexeme = Number.IsScientificNotation2(source);
-        Assert.Equal(expectedSuccess, lexeme.Matched);
-        if (lexeme.Matched)
-        {
-            Assert.Equal(source, lexeme);
-        }
-    }
-
-    [Theory]
-    [InlineData("1", false)]
-    [InlineData("12", false)]
-    [InlineData("123456", false)]
-    [InlineData("+1", false)]
-    [InlineData("-1", false)]
-    [InlineData("+123", false)]
-    [InlineData("-123", false)]
-
-    [InlineData("1.0", false)]
-    [InlineData("12.0", false)]
-    [InlineData("123456.0", false)]
-    [InlineData("+1.0", false)]
-    [InlineData("-1.0", false)]
-    [InlineData("+123.0", false)]
-    [InlineData("-123.0", false)]
-
-    [InlineData("1E1", false)]
-
     [InlineData("1.0E1", false)]
     [InlineData("1.0e1", false)]
     [InlineData("+1.0E1", false)]
@@ -338,55 +211,6 @@ public sealed class NumericTests
     public void IsHexNotation(string source, bool expectedSuccess)
     {
         var lexeme = Number.IsHexNotation(source);
-        Assert.Equal(expectedSuccess, lexeme.Matched);
-        if (lexeme.Matched)
-        {
-            Assert.Equal(source, lexeme);
-        }
-    }
-
-    [Theory]
-    [InlineData("1", false)]
-    [InlineData("12", false)]
-    [InlineData("123456", false)]
-    [InlineData("+1", false)]
-    [InlineData("-1", false)]
-    [InlineData("+123", false)]
-    [InlineData("-123", false)]
-
-    [InlineData("1.0", false)]
-    [InlineData("12.0", false)]
-    [InlineData("123456.0", false)]
-    [InlineData("+1.0", false)]
-    [InlineData("-1.0", false)]
-    [InlineData("+123.0", false)]
-    [InlineData("-123.0", false)]
-
-    [InlineData("1E1", false)]
-
-    [InlineData("1.0E1", false)]
-    [InlineData("1.0e1", false)]
-    [InlineData("+1.0E1", false)]
-    [InlineData("-1.0e1", false)]
-    [InlineData("1.0E+1", false)]
-    [InlineData("1.0e-1", false)]
-
-    [InlineData("-", false)]
-    [InlineData("+", false)]
-    [InlineData("a", false)]
-    [InlineData(" ", false)]
-
-    [InlineData("0x00", true)]
-    [InlineData("0x01", true)]
-    [InlineData("1x00", false)]
-    [InlineData("0xFF", true)]
-    [InlineData("0xff", true)]
-    [InlineData("0xf", true)]
-    [InlineData("0xF", true)]
-    [InlineData("0xfFfffFFaaff", true)]
-    public void IsHexNotation2(string source, bool expectedSuccess)
-    {
-        var lexeme = Number.IsHexNotation2(source);
         Assert.Equal(expectedSuccess, lexeme.Matched);
         if (lexeme.Matched)
         {
